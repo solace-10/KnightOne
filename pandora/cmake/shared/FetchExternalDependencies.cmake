e963f0bb878c17fe9fb2222170f3e39799aa1df6
Pedro Nunes <pnunes515@protonmail.com>
2024-06-13 17:14:32 +0100

    Basic Dawn app.

--- pandora/cmake/shared/FetchExternalDependencies.cmake
+++ pandora/cmake/shared/FetchExternalDependencies.cmake
@@ -4,10 +4,14 @@ message(STATUS "Pandora: fetching external dependencies...")
 
 set(FETCHCONTENT_QUIET FALSE)
 
-FetchContent_Declare(
-  dawn
-  GIT_REPOSITORY https://github.com/google/dawn.git
-  GIT_TAG 57c158000cb079a03f19c625d2727cfb628c8e5e
-  GIT_PROGRESS TRUE
-)
-FetchContent_MakeAvailable(dawn)
+# By default Dawn uses Chromium's build system and dependency management.
+# Setting DAWN_FETCH_DEPENDENCIES clones the dependencies repositories.
+#set(DAWN_FETCH_DEPENDENCIES ON)
+#FetchContent_Declare(
+#  dawn
+#  GIT_REPOSITORY https://github.com/google/dawn.git
+#  GIT_TAG 1762328886136518b56961f8388d90472dc2c973
+#  GIT_PROGRESS TRUE
+#  EXCLUDE_FROM_ALL
+#)
+#FetchContent_MakeAvailable(dawn)
