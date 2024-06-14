18d936a2f86dbdcb2364802b615c5650a2f0106c
Pedro Nunes <pnunes515@protonmail.com>
2024-06-14 22:57:16 +0100

    pandora lib now built as an intermediate dependency

--- game/cmake/Findpandora.cmake
+++ game/cmake/Findpandora.cmake
@@ -1,8 +1,2 @@
 set(PANDORA_INCLUDE_DIRS "${pandora_SOURCE_DIR}/src/")
-set(PANDORA_LIBRARY_DIRS "${pandora_SOURCE_DIR}/libs/pandora/${CMAKE_HOST_SYSTEM_NAME}/${CMAKE_HOST_SYSTEM_PROCESSOR}/${CMAKE_BUILD_TYPE}")
-
-#if(WIN32)
-#    set(PANDORA_CRASH_HANDLER "${pandora_SOURCE_DIR}/../build/$<IF:$<CONFIG:Debug>,Debug,Release>/crashpad_handler.exe")
-#else()
-#    set(PANDORA_CRASH_HANDLER "${pandora_SOURCE_DIR}/../build/crashpad_handler")
-#endif()
+set(PANDORA_LIBRARY_DIRS "${CMAKE_BINARY_DIR}/_deps/pandora/lib")
