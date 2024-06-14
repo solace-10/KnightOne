efab9cb84b2962e37f644dea3532c79657621e56
Pedro Nunes <pnunes515@protonmail.com>
2024-06-14 15:10:57 +0100

    Using FetchContent rather than a submodule for dawn.

--- pandora/cmake/shared/FetchExternalDependencies.cmake
+++ pandora/cmake/shared/FetchExternalDependencies.cmake
@@ -6,12 +6,14 @@ set(FETCHCONTENT_QUIET FALSE)
 
 # By default Dawn uses Chromium's build system and dependency management.
 # Setting DAWN_FETCH_DEPENDENCIES clones the dependencies repositories.
-#set(DAWN_FETCH_DEPENDENCIES ON)
-#FetchContent_Declare(
-#  dawn
-#  GIT_REPOSITORY https://github.com/google/dawn.git
-#  GIT_TAG 1762328886136518b56961f8388d90472dc2c973
-#  GIT_PROGRESS TRUE
-#  EXCLUDE_FROM_ALL
-#)
-#FetchContent_MakeAvailable(dawn)
+set(DAWN_FETCH_DEPENDENCIES ON)
+FetchContent_Declare(
+    dawn
+    GIT_REPOSITORY https://github.com/google/dawn.git
+    GIT_TAG 2a26b19b72815804d0c3b91def9b82a2dc2178a5 # HEAD as of Jun 14, 2024
+    GIT_PROGRESS TRUE
+    GIT_SUBMODULES ""
+    SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/ext/dawn
+    EXCLUDE_FROM_ALL
+)
+FetchContent_MakeAvailable(dawn)
