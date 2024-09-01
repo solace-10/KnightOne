7fb51881c6fbdfe8893e202acea936cdf172dba9
Pedro Nunes <pnunes515@protonmail.com>
2024-09-01 17:49:15 +0100

    Missed changes to Forge.

--- forge/src/main.cpp
+++ forge/src/main.cpp
@@ -5,8 +5,13 @@ int main()
 {
     using namespace WingsOfSteel;
 
+    Pandora::WindowSettings windowSettings;
+    windowSettings.SetSize(900, 600);
+    windowSettings.SetTitle("Forge");
+
     Forge::Forge forge;
     Pandora::Initialize(
+        windowSettings,
         [&forge](){ forge.Initialize(); },
         [&forge](float delta){ forge.Update(delta); },
         [&forge](){ forge.Shutdown(); }
